8dbd4d5e8c84b72b866627267fc52df80db57693
Cristian Farias <cristian.stedwards@gmail.com>
2021-07-27 20:24:05 -0500

    update

--- game.js
+++ game.js
@@ -57,12 +57,12 @@ startGame = () => {
 
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+        localStorage.setItem("mostRecentScore", score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign("/end.html")
     }
 
-    questionCounter ++
+    questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
     progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
@@ -72,8 +72,9 @@ getNewQuestion = () => {
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
-        choice.innerText = currentQuestion ['choice' + number]
+        choice.innerText = currentQuestion['choice' + number]
     })
+
     availableQuestions.splice(questionsIndex, 1)
 
     acceptingAnswers = true
@@ -98,7 +99,6 @@ choices.forEach(choice => {
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-
         }, 1000)
     })
 })
